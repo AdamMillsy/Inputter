01dc9753566166e4845bf67ce5e960c6791bf3b3
Adam Mills <adamm1404@outlook.com>
2025-04-09 01:28:59 +0100

    Built and published by Moonwave

---
+++ assets/js/74f5d79b.c3a5f9c6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[835],{4410:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/AdamMillsy/Inputter/blob/master","baseUrl":"/Inputter/","classOrder":[],"apiCategories":[]}')}}]);
